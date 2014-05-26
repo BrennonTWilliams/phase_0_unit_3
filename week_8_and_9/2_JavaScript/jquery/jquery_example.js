0f7891fbd58727ae563307e5588237f6e1dc0958
Brennon Williams <brennonw@gmail.com>
2014-05-25 19:30:37 -0700

    completed jQuery javascript problem

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -3,23 +3,39 @@ $(document).ready(function(){
 //RELEASE 0: 
   //Link this script and the jQuery library to the jquery_example.html file and analyze what this code does. 
  
-$('body').css({'background-color': 'pink'})
+$('body').css({'background-color': 'green'})
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
- 
+  var bodyElement = $('body');
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
- 
+$('h1').css({
+	'color': 'yellow',
+	'border': 'dotted', 
+	'visibility': 'visible'});
+
+ $('div.mascot > h1').html('Chorus Frogs');
  
 //RELEASE 3: Event Listener
-  // Add the code for the event listener here 
+  // Add the code for the e'vent listener here 
  
+ $('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://www.borealforest.org/reptiles/chorus_frog.jpg')
+  })
  
+ $('img').on('mouseleave', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'https://ed.fnal.gov/projects/frogs/graphics/chorus_frog_i.jpg')
+  })
 //RELEASE 4 : Experiment on your own
  
- 
+ $('img').on('click', function(e){
+     e.preventDefault()
+    $(this).fadeOut()
+  })
  
  
  
